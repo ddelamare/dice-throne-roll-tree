6f11683a1d7da67ec5243dff9d4c64f9072cf809
ddelamare <ddelam93@gmail.com>
2026-05-31 05:00:44 +0000

    Updated default deltas

--- src/DiceThroneApi/Models/EvaluationConfig.cs
+++ src/DiceThroneApi/Models/EvaluationConfig.cs
@@ -5,9 +5,9 @@ public class EvaluationConfig
     // Values used to convert non-damage outcomes into the same "delta" unit.
     // Default: heal=1, card=1, cp=1, default token value=2.
     public double HealValue { get; set; } = 1.0;
-    public double CardValue { get; set; } = 1.0;
+    public double CardValue { get; set; } = 3.0;
     public double CpValue { get; set; } = 1.0;
     public double DefaultTokenValue { get; set; } = 2.0;
-    public double EnemyDefenseDelta { get; set; } = 2.5; // How much to subtract from damage when calculating expected value (to account for enemy defense)
+    public double EnemyDefenseDelta { get; set; } = 3.0; // How much to subtract from damage when calculating expected value (to account for enemy defense)
     public Dictionary<string, double> TokenValues { get; set; } = new();
 }
